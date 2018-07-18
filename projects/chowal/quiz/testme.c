e85fd3db10a75d0b53b65bd1d2204ccf67e54b4e
chowal <chowal@oregonstate.edu>
2018-07-17 19:24:34 -0700

    adding Random test quiz

---
+++ projects/chowal/quiz/testme.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // 32 to 125
+    int num = (rand() % 94) + 32;
+    char result = num;
+    return result;
+}
+
+char *inputString()
+{
+    int length = 5;
+    char *result = NULL;
+    result = malloc(sizeof(char) * length); 
+    int i;
+
+    // 97 to 122
+    for(i = 0; i < length; i++){
+        result[i] = (rand() % (122 - 97)) + 97;
+    }   
+
+    return result;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' ' && state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    //char x = inputChar();
+    //char *x = inputString();
+    //printf("ANS is %c\n", x);
+    return 0;
+}
