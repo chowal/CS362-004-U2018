1e75c45e27c52b1f4e2a2255a387922e5b841e62
chowal <chowal@oregonstate.edu>
2018-07-27 16:44:00 -0700

    adding new random tests

---
+++ projects/chowal/dominion/randomtestadventurer.c
@@ -0,0 +1,55 @@
+/*
+ *
+ * Test for adventure card
+ *
+ *
+ *
+ */
+
+#include "dominion.c"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+#include <math.h>
+
+#define TESTCARD "great_hall"
+int passing = 0;
+int failing = 0;
+
+char spacing[20] = "~~~~~~~~~~~~~~~~~~~~";
+
+void assertInt(int x, int y){
+    if (x == y){
+        printf("PASS\n");
+        passing += 1;
+    }
+    else{
+        printf("TEST FAILED\n");
+        failing += 1;
+    }   
+}
+
+
+int main() {
+    int newCards = 0;
+    int discarded = 1;
+    int action = 1;
+    int shuffledCards = 0;
+
+    int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+    int seed = 1000;
+    int numPlayers = 2;
+    int thisPlayer = 0;
+	struct gameState G, testG;
+	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, great_hall};
+
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
+    memcpy(&testG, &G, sizeof(struct gameState));
+	cardEffect(great_hall, choice1, choice2, choice3, &testG, handpos, &bonus);
+
+}
